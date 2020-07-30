feefb3e8738f3464749611bfe22c09d2f44b8fa8
e <luochenghang>
2020-07-30 21:07:27 +0800

    login

--- pages/my/login/login.js
+++ pages/my/login/login.js
@@ -1,66 +1,59 @@
-// pages/my/login/login.js
+const app = getApp()
+const ApiManager = require('../../../api/ApiManage.js')
+const ApiConst = require('../../../api/ApiConst.js')
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    phone: '',
+    password: ''
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 获取输入账号 
+  phoneInput: function(e) {
+    this.setData({
+      phone: e.detail.value
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 获取输入密码 
+  passwordInput: function(e) {
+    this.setData({
+      password: e.detail.value
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 登录 
+  login: function() {
+    if (this.data.phone.length == 0 || this.data.password.length == 0) {
+      wx.showToast({
+        title: '用户名和密码不能为空',
+        icon: 'loading',
+        duration: 2000
+      })
+    } else {
+      let that = this
+      let requestData = {
+        url: ApiConst.loginChart,
+        data: {
+          'userName': this.data.phone,
+          'password': this.data.password,
+        }
+      }
+      ApiManager.send(requestData, 'POST').then(res => {
+        if (res.data.code == 1000) {
+          // 这里修改成跳转的页面 
+          wx.showToast({
+            title: '登录成功',
+            icon: 'success',
+            duration: 2000
+          })
+          setTimeout(function() {
+            wx.navigateTo({
+              url: '/pages/my/chart/chart',
+            })
+          }, 1000)
+        }
+      })
+    }
   }
 })
